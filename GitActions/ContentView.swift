69fec7854eb4ca255175494cdb319e04b59df0ec
LukasSkrivanek <luky.skrivos@gmail.com>
2024-11-08 12:50:53 +0100

    [Feature] Subscribe button

--- GitActions/ContentView.swift
+++ GitActions/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                         Button {
                             
                         } label: {
-                            Text("Click me")
+                            Text("Subscribe now")
                         }
                     }
                 }
