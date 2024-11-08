a740a20e66f57ba0edc3c775a47057c2e2df6531
LukasSkrivanek <luky.skrivos@gmail.com>
2024-11-08 12:01:56 +0100

    [Feature] Update Content View

--- GitActions/ContentView.swift
+++ GitActions/ContentView.swift
@@ -10,16 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "house.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Swiftul Thinking")
+            Text("Some new title!")
             
             Button {
                 
             } label: {
-                Text("Subscribe now")
+                Text("Click me")
             }
 
         }
