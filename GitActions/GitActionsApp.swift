7a59e5db2288e0fe8a479bf7c443a8f9efd38d7a
LukasSkrivanek <luky.skrivos@gmail.com>
2024-11-08 10:32:32 +0100

    Initial Commit

---
+++ GitActions/GitActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitActionsApp.swift
+//  GitActions
+//
+//  Created by macbook on 08.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitActionsTests/GitActionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitActionsTests.swift
+//  GitActionsTests
+//
+//  Created by macbook on 08.11.2024.
+//
+
+import Testing
+@testable import GitActions
+
+struct GitActionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
