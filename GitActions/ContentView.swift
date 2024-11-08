dcb58f43fca0463ab7eff6e2ca422c1d3744bac6
LukasSkrivanek <luky.skrivos@gmail.com>
2024-11-08 10:53:56 +0100

    [Feature] Buton

--- GitActions/ContentView.swift
+++ GitActions/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button {
+                
+            } label: {
+                Text("Subscribe now")
+            }
+
         }
         .padding()
     }
