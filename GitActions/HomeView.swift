1dcc858c7859ec00625824e1698fad9c07eb0f96
LukasSkrivanek <luky.skrivos@gmail.com>
2024-11-08 12:24:40 +0100

    [Feature] New File

---
+++ GitActions/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitActions
+//
+//  Created by macbook on 08.11.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
