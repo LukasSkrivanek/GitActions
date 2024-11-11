4e35f9c600c124a7d320cffe90f500e4e53df764
LukasSkrivanek <luky.skrivos@gmail.com>
2024-11-11 10:13:16 +0100

    [Feature]
    add profile view

---
+++ GitActions/ProfileView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileView.swift
+//  GitActions
+//
+//  Created by macbook on 11.11.2024.
+//
+import SwiftUI
+
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
